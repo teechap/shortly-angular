dc1b4dc85ab02e8c13d9eedebe845c93501bc669
teechap <travisechapman@gmail.com>
2015-01-17 12:12:52 -0800

    Finished

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,8 +1,12 @@
 angular.module('shortly.nav', [])
 
-.controller('NavController', function ($scope, $location) {
-  $scope.isActive = function(route){
-    $scope.path = $location.path();
-    return $location.path() === route;
+.controller('NavController', function ($scope, $window) {
+  $scope.signedIn = function(){
+    var thisToken = $window.localStorage.getItem('com.shortly') === null ? false : true;
+    return thisToken;
   };
+
+  $scope.logout = function(){
+    $window.localStorage.removeItem('com.shortly');
+  }
 });
